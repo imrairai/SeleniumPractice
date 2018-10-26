f2a6b0e65e7587636d1db38b60bc31d485103693
rguzman <rguzman@LON-QA-RG-01.qa.palantirsolutions.com>
2018-10-26 16:42:58 +0800

    new commit

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -5,6 +5,14 @@
  */
 package test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 /**
  *
  * @author rguzman
@@ -16,6 +24,24 @@ public class Test {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        String exePath = "C:\\Drivers\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", exePath);
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://ipsqa08.qa.palantirsolutions.com/");
+        driver.findElement(By.id("username")).sendKeys("Administrator");
+        driver.findElement(By.cssSelector("button.form-control.btn-primary")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("li.ng-binding:nth-child(2)")));
+        
+        driver.findElement(By.cssSelector("li.ng-binding:nth-child(2)")).click();
+        
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("Password")));
+        driver.findElement(By.name("Password")).sendKeys("Administrator");
+        driver.findElement(By.name("button")).click();
+
     }
-    
+
 }
